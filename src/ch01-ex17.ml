f10bf3acb5b26802d9f36da8850dd07931621031
mukka <mukka@c3607132-0dcd-42ba-a3c3-24b50010b387>
2010-02-28 14:13:41 +0000

    * ch01.tex   add partial solution of 1.14. correct the solution of 1.15 and add some   footnote. add a todo mark on 1.13.
    * src/ch01-ex17.*
      handle '0 x n' cases more efficiently.
    
    
    git-svn-id: svn+ssh://lab/home/svn/sicp/trunk@20 c3607132-0dcd-42ba-a3c3-24b50010b387

--- src/ch01-ex17.ml
+++ src/ch01-ex17.ml
@@ -5,8 +5,11 @@ let halve n = n / 2
 let is_even n = n mod 2 = 0
 
 let rec fast_mul a b =
-  match b with
-  | 0 -> 0
-  | 1 -> a
-  | _ when is_even b -> fast_mul (double a) (halve b)
-  | _ -> a + (fast_mul a (b - 1))
+  if a = 0 || b = 0 then
+    0
+  else if b = 1 then
+    a
+  else if is_even b then
+    fast_mul (double a) (halve b)
+  else
+    a + (fast_mul a (b - 1))
--- src/ch01-ex17.ss
+++ src/ch01-ex17.ss
@@ -5,7 +5,7 @@
 (define (even? n) (= (remainder n 2) 0))
 
 (define (fast-mul a b)
-  (cond ((= b 0) 0)
+  (cond ((or (= a 0) (= b 0)) 0)
         ((= b 1) a)
         ((even? b) (fast-mul (double a) (halve b)))
         (else (+ a (fast-mul a (- b 1))))))
